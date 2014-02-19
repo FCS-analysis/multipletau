01c72f1260f425eddadcacf2a31328e033baac8f
scholich <andre.scholich@gmail.com>
2014-02-19 16:10:41 +0100

    Commit one build of the documentation for reference and then add it to gitignore.

---
+++ doc/make.py
@@ -0,0 +1,14 @@
+import os
+import sys
+sys.path.insert(0, '..')
+import shutil
+import fnmatch
+
+
+os.system('make html')
+
+# os.system('make latex')
+# olddir = os.curdir
+# os.chdir('_build\\latex\\')
+# os.system('pdflatex multipletau.tex')
+# os.chdir(olddir)
