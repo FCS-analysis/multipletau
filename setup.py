56daa8478a49b092d0d79032b1e5a72f0d63f9ab
5k41 <paul.mueller@biotec.tu-dresden.de>
2014-05-23 14:47:38 +0200

    updated setup.yp

--- setup.py
+++ setup.py
@@ -18,7 +18,22 @@ setup(
     license="OpenBSD",
     description='A multiple-tau algorithm for Python/NumPy.',
     long_description=open(join(dirname(__file__), 'README.txt')).read(),
-    install_requires=["NumPy >= 1.5.1"]
+    install_requires=["NumPy >= 1.5.1"],
+    keywords=["multiple", "tau", "FCS", "correlation", "spectroscopy",
+              "fluorescence"],
+    extras_require={
+                    'doc': ['sphinx']
+                   },
+    classifiers= [
+        'Operating System :: Unix',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Intended Audience :: Science/Research'
+                 ],
+    platforms=['ALL']
     )
 
 
