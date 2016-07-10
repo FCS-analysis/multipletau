8d81e205236d57ee5112349b424b7e76dc066e6a
Paul Müller <paul.mueller@biotec.tu-dresden.de>
2016-07-10 15:26:34 +0200

    update example

--- examples/compare_correlation_methods.py
+++ examples/compare_correlation_methods.py
@@ -37,12 +37,10 @@ def compare_corr():
     # time factor
     taudiff *= deltat
 
-    ##
     ## Autocorrelation
-    ##
     print("Creating noise for autocorrelation")
     data = noise_exponential(N, taudiff, deltat=deltat)
-    data += - np.average(data)
+    data -= np.average(data)
     if normalize:
         data += countrate
     # multipletau
@@ -53,71 +51,66 @@ def compare_corr():
         print("Performing autocorrelation (numpy).")
         Gd = correlate_numpy(data, data, deltat=deltat,
                              normalize=normalize)
-    # Calculate the expected curve
-    x = G[:,0]
+    
+    ## Cross-correlation
+    print("Creating noise for cross-correlation")
+    a, v = noise_cross_exponential(N, taudiff, deltat=deltat)
+    a -= np.average(a)
+    v -= np.average(v)
+    if normalize:
+        a += countrate
+        v += countrate
+    Gccforw = correlate(a, v, deltat=deltat, normalize=normalize) # forward
+    Gccback = correlate(v, a, deltat=deltat, normalize=normalize) # backward
+    if len(a) < 1e5:
+        print("Performing cross-correlation (numpy).")
+        Gdccforw = correlate_numpy(a, v, deltat=deltat, normalize=normalize)
+    
+    ## Calculate the model curve for cross-correlation
+    xcc = Gd[:,0]
+    ampcc = np.correlate(a-np.average(a), v-np.average(v), mode="valid")
+    if normalize:
+        ampcc /= len(a) * countrate**2
+    ycc = ampcc*np.exp(-xcc/taudiff)
+
+    ## Calculate the model curve for autocorrelation
+    x = Gd[:,0]
     amp = np.correlate(data-np.average(data), data-np.average(data),
                        mode="valid")
     if normalize:
         amp /= len(data) * countrate**2
     y = amp*np.exp(-x/taudiff)
 
-    ##
-    ## Cross-correlation
-    ##
-    print("Creating noise for cross-correlation")
-    a, v = noise_cross_exponential(N, taudiff, deltat=deltat)
-    a += - np.average(a)
-    v += - np.average(v)
-    if normalize:
-        a += countrate
-        v += countrate
-    # multipletau
-    Gccforw = correlate(a, v, deltat=deltat, normalize=normalize)
-    Gccback = correlate(v, a, deltat=deltat, normalize=normalize)
-    if len(a) < 1e5:
-        print("Performing autocorrelation (numpy).")
-        Gdccforw = correlate_numpy(a, v, deltat=deltat, normalize=normalize)
-    # Calculate the expected curve
-    xcc = Gccforw[:,0]
-    ampcc = np.correlate(a-np.average(a), v-np.average(v), mode="valid")
 
-    if normalize:
-        ampcc /= len(a) * countrate**2
-    ycc = ampcc*np.exp(-xcc/taudiff)
-
-
-    ##
     ## Plotting
-    ##
-
     # AC
     fig = plt.figure()
     fig.canvas.set_window_title('testing multipletau')
     ax = fig.add_subplot(2,1,1)
     ax.set_xscale('log')
-    plt.plot(x, y, "g-", label="input model")
-    plt.plot(G[:,0], G[:,1], "r-", label="autocorrelate")
     if len(data) < 1e5:
-        plt.plot(Gd[:,0], Gd[:,1] , "b--", label="correlate (numpy)")
+        plt.plot(Gd[:,0], Gd[:,1] , "-", color="gray", label="correlate (numpy)")
+    plt.plot(x, y, "g-", label="input model")
+    plt.plot(G[:,0], G[:,1], "-",  color="#B60000", label="autocorrelate")
     plt.xlabel("lag channel")
     plt.ylabel("autocorrelation")
     plt.legend(loc=0, fontsize='small')
     plt.ylim( -amp*.2, amp*1.2)
+    plt.xlim( Gd[0,0], Gd[-1,0])
 
-
-    ## CC
+    # CC
     ax = fig.add_subplot(2,1,2)
     ax.set_xscale('log')
-    plt.plot(xcc, ycc, "g-", label="input model")
-    plt.plot(Gccforw[:,0], Gccforw[:,1], "r-", label="forward")
     if len(data) < 1e5:
-        plt.plot(Gdccforw[:,0], Gdccforw[:,1] , "b--", label="forward (numpy)")
-    plt.plot(Gccback[:,0], Gccback[:,1], "r--", label="backward")
+        plt.plot(Gdccforw[:,0], Gdccforw[:,1] , "-", color="gray", label="forward (numpy)")
+    plt.plot(xcc, ycc, "g-", label="input model")
+    plt.plot(Gccforw[:,0], Gccforw[:,1], "-", color="#B60000", label="forward")
+    plt.plot(Gccback[:,0], Gccback[:,1], "-", color="#5D00B6", label="backward")
     plt.xlabel("lag channel")
     plt.ylabel("cross-correlation")
     plt.legend(loc=0, fontsize='small')
-
     plt.ylim( -ampcc*.2, ampcc*1.2)
+    plt.xlim( Gd[0,0], Gd[-1,0])
     plt.tight_layout()
 
     savename = __file__[:-3]+".png"
