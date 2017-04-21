d3d7f4b700699609850dfdb22157f59e22b75c29
paulmueller <paulmueller@users.noreply.github.com>
2017-04-21 10:33:37 +0200

    typo

--- multipletau/_multipletau.py
+++ multipletau/_multipletau.py
@@ -100,7 +100,7 @@ def autocorrelate(a, m=16, deltat=1, normalize=False,
     Examples
     --------
     >>> from multipletau import autocorrelate
-    >>> autocorrelate(range(42), m=2, dtype=np.float__)
+    >>> autocorrelate(range(42), m=2, dtype=np.float_)
     array([[  0.00000000e+00,   2.38210000e+04],
            [  1.00000000e+00,   2.29600000e+04],
            [  2.00000000e+00,   2.21000000e+04],
